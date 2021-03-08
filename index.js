677ffc70deba2dfa83a8d716b4e7f0092f180098
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-08 21:58:46 +0900

    #7 BodyParser & PostMan & 회원 가입 기능

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// applicatiopn/json
+app.use(bodyParser.json());
+
 // 몽구스 연결 : 에러 => 다운그레이드
 const mongoose = require("mongoose");
 mongoose
@@ -18,5 +26,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Node World"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
