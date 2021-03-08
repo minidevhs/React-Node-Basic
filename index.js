6d2eb7cde0534fd8bf30a44080715dd6d30f1326
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-09 00:22:36 +0900

    #14 로그아웃 기능

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
-app.get("api/users/auth", auth, (req, res) => {
+app.get("/api/users/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -82,6 +82,15 @@ app.get("api/users/auth", auth, (req, res) => {
     password: req.user.password,
     role: req.user.role,
     image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
   });
 });
 
