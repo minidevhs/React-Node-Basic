237fc67a898467f30be7eb27dd0ee1caa32c6e6c
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-10 23:00:31 +0900

    #32 로그아웃

--- client/src/components/views/LadingPage/LadingPage.js
+++ client/src/components/views/LadingPage/LadingPage.js
@@ -1,12 +1,24 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { PromiseProvider } from "mongoose";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
       console.log(response.data);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        alert("로그아웃이 완료 되었습니다.");
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 하는데 실패 했습니다.");
+      }
+    });
+  };
 
   return (
     <div
@@ -18,7 +30,8 @@ function LandingPage() {
         height: "100vh",
       }}
     >
-      시작 페이지
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
